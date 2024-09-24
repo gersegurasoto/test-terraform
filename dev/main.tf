4e9582757c2c76feeaefb93fc48582ed0ea2f720
gersegurasoto <152655131+gersegurasoto@users.noreply.github.com>
2024-09-24 16:39:02 -0600

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -2,7 +2,7 @@ module "rds" {
   source            = "../modules/rds"
   db_identifier     = "mi-rds-db"
   engine            = "mysql"  # Cambia según el motor que necesites
-  instance_class    = "db.t2.micro"  # Cambia según tus necesidades
+  instance_class    = "db.t3.micro"  # Cambia según tus necesidades
   allocated_storage  = 20  # Cambia según tus necesidades
   username          = "admin"
   password          = "tu_password"  # Asegúrate de manejar esto de forma segura
